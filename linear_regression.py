3b6c40144e0b3aabde271907dc77fea9a7c8556c
Sahej17 <43532447+Sahej17@users.noreply.github.com>
2019-09-02 22:55:20 +0530

    Added pickling and scaling part

--- linear_regression.py
+++ linear_regression.py
@@ -6,6 +6,7 @@ from sklearn import preprocessing, svm, model_selection
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
 
 style.use('ggplot')
 
@@ -35,12 +36,17 @@ X = X[:-forecast_out:]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
-y = np.array(df['label'])
 
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
 clf.fit  (X_train, y_train)
+with open('linearregression.pickle','wb') as f:
+	pickle.dump(clf,f)
+
+pickle_in = open('linearregression.pickle','rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
 print(forecast_set, accuracy, forecast_out)
